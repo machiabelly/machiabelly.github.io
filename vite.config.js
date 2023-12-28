20a4e5a7da14a496f894a541c61bc0e648fdd347
machiabelly <106708720+machiabelly@users.noreply.github.com>
2023-12-28 17:49:35 -0500

    Revert "Revert "Update README.md and vite.config.js""
    This reverts commit dfdc44c00db4c16b9f253d69fd4c7e635e552e9d.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import {visualizer} from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/landing_page',
+	base: '/',
 	plugins: [
 		react(),
 		visualizer({
